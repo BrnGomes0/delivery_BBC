fa3925989fff727279ed083fe6eef7b2f6d43c7d
BrnGomes0 <brunownog05@gmail.com>
2024-05-16 08:11:19 -0300

    :hammer: fix: fix the Order controller

--- back-end/src/main/java/bbc/api/application/controller/OrderController.java
+++ back-end/src/main/java/bbc/api/application/controller/OrderController.java
@@ -6,9 +6,8 @@ import bbc.api.application.dto.order.DataListOrder;
 import bbc.api.application.dto.order.DataRegisterOrder;
 import bbc.api.application.entity.Order;
 import bbc.api.application.infra.exception.OrderNotFoundException;
-import bbc.api.application.service.utils.order.OrderService;
+import bbc.api.application.service.order.OrderService;
 import jakarta.validation.Valid;
-import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -18,34 +17,26 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.Optional;
-
 @RestController
 @RequestMapping("/order")
 public class OrderController {
 
     @Autowired
     private OrderService orderService;
-
-    // POST
     @PostMapping
     public ResponseEntity<DataDetailOrder> registerOder(@RequestBody @Valid DataRegisterOrder dataRegisterOrder, UriComponentsBuilder uriBuilder){
         var oder = orderService.createAOrder(dataRegisterOrder);
         var uri = uriBuilder.path("/order/{order_id}").build(oder.order_id());
         return ResponseEntity.created(uri).body(oder);
     }
-
-    // GET List
     @GetMapping("/listOrders")
     public ResponseEntity<Page<DataListOrder>> listTheOrders(@PageableDefault(size = 8, sort = {"name"})Pageable pageable){
         var listOfOrders = orderService.listOfOrders(pageable);
         return ResponseEntity.status(HttpStatus.OK).body(listOfOrders);
     }
-
-    // PATCH
-//    @PatchMapping("/{order_id}/confirm")
-//    public ResponseEntity<Optional<Order>> confirmOrder(@PathVariable Long order_id) throws OrderNotFoundException {
-//        var confirmOrder = orderService.confirmOrder(order_id);
-//        return ResponseEntity.status(HttpStatus.OK).body(Optional.ofNullable(confirmOrder));
-//    }
+    @PatchMapping("/{order_id}/confirm")
+    public ResponseEntity<Order> confirmOrder(@PathVariable Long order_id) throws OrderNotFoundException {
+        var confirmOrder = orderService.confirmOrder(order_id);
+        return ResponseEntity.status(HttpStatus.OK).body(confirmOrder);
+    }
 }
